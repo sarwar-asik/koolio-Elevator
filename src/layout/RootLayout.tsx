910485a9255af8ce22461a71487aef1db9d29cd3
sarwar-asik <sarwarasik@gmail.com>
2024-03-20 21:48:17 +0600

    page/shared: navbar created for rootLayout

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Home/shared/Navbar/Navbar";
 
 
 export default function RootLayout() {
-  return (
-    <div>RootLayout</div>
-  )
+    return (
+        <div>
+            <Navbar />
+            <Outlet />
+
+        </div>
+    )
 }
---
+++ src/pages/Home/shared/Navbar/Navbar.tsx
@@ -0,0 +1,90 @@
+import  { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { IoClose, IoMenu } from "react-icons/io5";
+import "./Navbar.css";
+
+const Navbar = () => {
+    const [showMenu, setShowMenu] = useState<boolean>(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
+    const closeMenuOnMobile = () => {
+        if (window.innerWidth <= 1150) {
+            setShowMenu(false);
+        }
+    };
+    return (
+        <header className="header">
+            <nav className="nav container">
+                <NavLink to="/" className="nav__logo">
+                    Navigation Bar
+                </NavLink>
+
+                <div
+                    className={`nav__menu ${showMenu ? "show-menu" : ""}`}
+                    id="nav-menu"
+                >
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <NavLink to="/" className="nav__link" onClick={closeMenuOnMobile}>
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="nav__item">
+                            <NavLink
+                                to="/news"
+                                className="nav__link"
+                                onClick={closeMenuOnMobile}
+                            >
+                                News
+                            </NavLink>
+                        </li>
+                        <li className="nav__item">
+                            <NavLink
+                                to="/about-us"
+                                className="nav__link"
+                                onClick={closeMenuOnMobile}
+                            >
+                                About Us
+                            </NavLink>
+                        </li>
+                        <li className="nav__item">
+                            <NavLink
+                                to="/favorite"
+                                className="nav__link"
+                                onClick={closeMenuOnMobile}
+                            >
+                                Favorite
+                            </NavLink>
+                        </li>
+                        <li className="nav__item">
+                            <NavLink
+                                to="/location"
+                                className="nav__link"
+                                onClick={closeMenuOnMobile}
+                            >
+                                Location
+                            </NavLink>
+                        </li>
+                        <li className="nav__item">
+                            <NavLink to="/get-started" className="nav__link nav__cta">
+                                Get Started
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <div className="nav__close" id="nav-close" onClick={toggleMenu}>
+                        <IoClose />
+                    </div>
+                </div>
+
+                <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
+                    <IoMenu />
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
